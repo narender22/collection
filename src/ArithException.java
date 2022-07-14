b4f462a43db0ee0f2ea6efa3080fd2b63092b8ab
narender <narendarkumar22@gmail.com>
2022-07-14 10:32:21 +0530

    Initial commit

--- src/ArithException.java
+++ src/ArithException.java
@@ -1,12 +1,16 @@
 public class ArithException {
     public static void main(String[] args) {
-        int a=5, b=0;
+        int a=5, b=1;
         try{
             System.out.println(a/b);
+            throw new NullPointerException("demo");
         }
         catch (ArithmeticException e){
             System.out.println(e.toString());
             System.out.println("Exception hapenned");
+        }
+        catch (NullPointerException e){
+            System.out.println("Throws Called");
         }
         finally {
             System.out.println("Code completed");
