9b535c6bc3a87a297b2eb55e3ca2de2b7a7eee3f
narender <narendarkumar22@gmail.com>
2022-07-15 11:36:27 +0530

    Initial commit

---
+++ src/MultipleInheritance.java
@@ -0,0 +1,21 @@
+interface PrintAble{
+    void print();
+}
+interface ShowAble{
+    void show();
+}
+
+class A7 implements PrintAble,ShowAble{
+    public void print(){
+        System.out.println("Printable called");
+    }
+    public void show(){
+        System.out.println("Showable called");
+    }
+}
+
+public class MultipleInheritance {
+    A7 obj = new A7();
+    obj.print();
+    obj.show();
+}
